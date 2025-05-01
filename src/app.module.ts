86f181c22eb4d6f220d72b201709326d97a25024
jsw <develjsw@gmail.com>
2025-05-01 22:04:56 +0900

    [env] 환경별 env 파일 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+let envFile = 'env.local';
+switch (process.env.NODE_ENV) {
+    case 'production':
+        envFile = 'env.production';
+        break;
+    case 'development':
+        envFile = 'env.development';
+        break;
+}
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: [path.resolve(__dirname, `../${envFile}`)],
+            isGlobal: true,
+            cache: true
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
