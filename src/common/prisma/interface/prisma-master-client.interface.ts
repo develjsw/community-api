a6141e64fe2cacbd302b76052bdc43d88340e90f
jsw <develjsw@gmail.com>
2025-05-09 00:50:39 +0900

    [interceptor] global response format interceptor 추가

--- src/common/prisma/interface/prisma-master-client.interface.ts
+++ src/common/prisma/interface/prisma-master-client.interface.ts
@@ -2,6 +2,7 @@ import { Prisma } from '@prisma-master-client';
 
 export const PRISMA_MASTER_CLIENT = Symbol('PrismaMasterClientInterface');
 
+// TODO: ORM 추상화는 오버엔지니어링일 수 있어서 빼는 방향도 고려
 export interface PrismaMasterClientInterface {
     post: Prisma.postDelegate;
     comment: Prisma.commentDelegate;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseFormatInterceptor } from './common/interceptor/response-format.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalInterceptors(new ResponseFormatInterceptor());
+
     await app.listen(9999);
 }
 bootstrap();
