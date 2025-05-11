d0531431583e9ee6309baab3b51186bb08d6a00e
jsw <develjsw@gmail.com>
2025-05-11 22:19:26 +0900

    [prisma] prisma slave service 추가

--- src/common/prisma/prisma.module.ts
+++ src/common/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaMasterClientService } from './service/prisma-master-client.service';
+import { PrismaSlaveClientService } from './service/prisma-slave-client.service';
 
 @Global()
 @Module({
-    providers: [PrismaMasterClientService],
-    exports: [PrismaMasterClientService]
+    providers: [PrismaMasterClientService, PrismaSlaveClientService],
+    exports: [PrismaMasterClientService, PrismaSlaveClientService]
 })
 export class PrismaModule {}
--- src/common/prisma/service/prisma-slave-client.service.ts
+++ src/common/prisma/service/prisma-slave-client.service.ts
@@ -1,6 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma-slave-client';
 
 @Injectable()
-export class PrismaSlaveClientService {
-    constructor() {}
+export class PrismaSlaveClientService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: process.env.SLAVE_DATABASE_URL
+                }
+            },
+            log: ['query', 'info', 'warn', 'error']
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async runInTransaction<T>(callback: (tx: Prisma.TransactionClient) => Promise<T>): Promise<T> {
+        return this.$transaction((tx: Prisma.TransactionClient) => callback(tx));
+    }
 }
