9bda17a772a6c9632c649e24a5cc63f3aa0576c5
jsw <develjsw@gmail.com>
2025-05-13 00:07:26 +0900

    [pipe] global pipe 설정값 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseFormatInterceptor } from './common/interceptor/response-format.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true
+        })
+    );
 
     app.useGlobalInterceptors(new ResponseFormatInterceptor());
 
--- src/post/interface/command/post-repository-command.interface.ts
+++ src/post/interface/command/post-repository-command.interface.ts
@@ -3,5 +3,5 @@ import { PostType } from '../../type/post.type';
 export const POST_COMMAND_REPOSITORY = Symbol('PostRepositoryCommandInterface');
 
 export interface PostRepositoryCommandInterface {
-    createPost(post: Partial<PostType>): Promise<PostType>;
+    createPost(post: Omit<PostType, 'postId' | 'createdAt' | 'updatedAt' | 'deletedAt'>): Promise<PostType>;
 }
